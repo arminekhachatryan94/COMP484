2b79ec4c8e192b07f09aad81d143f4dbc5ddb8af
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2017-10-03 23:03:38 -0700

    letter comparison

--- Lab2/jquery.js
+++ Lab2/jquery.js
@@ -1,22 +1,58 @@
 // jquery
 
+var misses = 0;
+
+var randWord;
+var wordDisplay=[];
 var s = false;
 var c = 0;
+
+var words = [
+    'PROGRAMMING',
+    'INHERITANCE',
+    'WEBSITES',
+    'JAVASCRIPT',
+    'NETWORKS',
+    'SECURITY',
+    'COMPUTERS',
+    'INTERNET',
+    'SOFTWARE',
+    'ENGINEERING',
+];
+
 // initially
 $(document).ready(function(){
+    randomize();
+    
     addLetters();
     
     $('#start').append('<btn id="startbtn" style="visibility: hidden;" class="btn-danger h3">Start New Game</btn>\n');
     
     $('.letter').click( function(){
+                
         $(this).css('visibility', 'hidden');
         c++;
-        console.log(c);
+        //console.log(c);
         
         if( c == 26 ){
             $('#startbtn').css('visibility', 'visible');
             s = true;
             c = 0;
+        }
+        
+        console.log(wordDisplay);
+        for( var i = 0; i < randWord.length; i++ ){
+            if($(this).text() == randWord[i]){
+                console.log('same');
+                wordDisplay=$('#word').text();
+                wordDisplay[i] = randWord[i];
+                
+                console.log(wordDisplay);
+                //wordDisplay=wordDisplay.join();
+                $('#word').text(wordDisplay);
+                console.log('#word').text();
+                wordDisplay=wordDisplay.split(' ');
+            }
         }
     });
     
@@ -40,4 +76,17 @@ var addLetters = function(){
 // make letters visible
 var showLetters = function(){
     $('.letter.letter').css('visibility', 'visible');
+}
+
+var randomize = function(){
+    randWord = words[Math.floor(Math.random()*words.length)];
+    
+    for( var i = 0; i < randWord.length; i++ ){
+        wordDisplay[i] = '_';
+    }
+    // $('#word').html('<div>'+randWord+'</div>');
+    
+    wordDisplay=wordDisplay.join(' ');
+    $('#word').append('<div>'+wordDisplay+'</div>');
+    wordDisplay=wordDisplay.split(' ');
 }
